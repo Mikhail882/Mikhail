43f9ac9528a08145435b8bed168ab61dc2164c16
Miklhail P <m.pojidaev@mail.ru>
2022-03-28 22:39:39 +0300

    Домашняя работа

--- src/com/skypro/Main.java
+++ src/com/skypro/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Привет, мир!");
+        System.out.println("Я стану Java разработчиком!");
     }
 }
